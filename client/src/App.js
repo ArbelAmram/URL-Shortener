d1747aefc70d176fb82a8a6b76536e9c6627a86b
Arbel Amram <arbelamram@gmail.com>
2022-06-29 13:28:11 +0300

    eliminate repetition of API base url

--- client/src/App.js
+++ client/src/App.js
@@ -14,14 +14,16 @@ function App() {
   const [urlId, setUrlId] = useState("")
   const [urlList, setUrlList] = useState([])
 
+  const API = 'http://localhost:5000'
+
   // generate url-id for original URL
   const generateUrl = async () => {
-    const response = await Axios.post('http://localhost:5000/generate', { originalUrl: originalUrl });
+    const response = await Axios.post(`${API}/generate`, { originalUrl: originalUrl });
     setUrlId(response.data);
   }
 
   const addToTable = () => {
-    Axios.post('http://localhost:5000/insert', {
+    Axios.post(`${API}/insert`, {
       originalUrl: originalUrl,
       shortUrl: urlId
     });
@@ -30,13 +32,13 @@ function App() {
   }
 
   useEffect(() => {
-    Axios.get('http://localhost:5000/read').then((response) => {
+    Axios.get(`${API}/read`).then((response) => {
     setUrlList(response.data);
     });
   }, [urlList])
 
   const deleteItem = (id) => {
-    Axios.delete(`http://localhost:5000/delete/${id}`)
+    Axios.delete(`${API}/delete/${id}`)
   }
 
   return (
