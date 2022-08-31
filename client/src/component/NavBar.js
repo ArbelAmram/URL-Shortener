557004a1360f963bce473480c69852042f67f461
Arbel Amram <arbelamram@gmail.com>
2022-08-31 09:04:18 +0300

    code cleanup

--- client/src/component/NavBar.js
+++ client/src/component/NavBar.js
@@ -24,27 +24,21 @@ const NavBar = () => {
           <li>
             <Link 
               to='/'
-              onClick={()=>{
-                setActiveBar('home')
-              }}
+              onClick={() => setActiveBar('home')}
               className={activeBar === 'home' ? 'active' : ''}
             >Home</Link>
           </li>
           <li>
             <Link 
               to='/new'
-              onClick={()=>{
-                setActiveBar('new')
-              }}
+              onClick={() => setActiveBar('new')}
               className={activeBar === 'new' ? 'active' : ''}
             >New</Link>
           </li>
           <li>
             <Link 
               to='/list'
-              onClick={()=>{
-                setActiveBar('list')
-              }}
+              onClick={() => setActiveBar('list')}
               className={activeBar === 'list' ? 'active' : ''}
             >List</Link>
           </li>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -10,8 +10,8 @@ const Home = () => {
             [SERVER_DOMAIN:"localhost:5000"/UNIQE_ID]<br />
             By accessing the short URL, the server will re-direct us to the original URL<br />
             The app will also count the number of entrances to the short URL and display it on the list of saved addresses<br />
-            Click on <span style={{ color: '#00bbf0'}}>New</span> to create a new short URL<br />
-            Click on <span style={{ color: '#00bbf0'}}>List</span> to view the list of saved URLs<br />
+            Click on <a href="/new" style={{ color: '#00bbf0'}}>New</a> to create a new short URL<br />
+            Click on <a href="/list" style={{ color: '#00bbf0'}}>List</a> to view the list of saved URLs<br />
             made by <span style={{ fontStyle: 'oblique', textDecoration: 'underline'}}>Arbel Amram</span>
         </p>
     </div>
